9c026626bcda34d56773ef243da5041c6b5fdb52
Manish Sharma <manish.sh.katha@gmail.com>
2025-10-06 16:37:35 +0530

    fix

--- app.config.js
+++ app.config.js
@@ -3,6 +3,7 @@ export default ({ config }) => ({
   extra: {
     // API_URL: process.env.API_URL || "http://192.168.1.100:5000/api",
     API_URL: "http://192.168.1.4:5000/api",
+      
     
   },
 });
